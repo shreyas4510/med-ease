402135908dafe49b47996bc75a6a385e292c1ba8
shreyas4510 <shreyasdeshmukh06@gmail.com>
2024-09-02 20:59:20 +0530

    frontend setup

---
+++ frontend-service/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import LandingPage from "./Pages/landingPage";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend-service/src/Pages/landingPage.tsx
@@ -0,0 +1,7 @@
+function LandingPage() {
+    return (
+        
+    )
+}
+
+export default LandingPage;

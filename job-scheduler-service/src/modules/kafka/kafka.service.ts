09f9353d0a33a34deed72f2ebe52abb5975ab11f
shreyas4510 <shreyasdeshmukh06@gmail.com>
2024-10-17 11:59:58 +0530

    job-scheduler changes for slots

--- job-scheduler-service/src/modules/kafka/kafka.service.ts
+++ job-scheduler-service/src/modules/kafka/kafka.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { Kafka } from 'kafkajs';
+import { SlotsService } from '../slots/slots.service';
 
 @Injectable()
 export class KafkaService {
     private kafka;
-    constructor() {
+    constructor(
+        private slotService: SlotsService
+    ) {
         this.kafka = new Kafka({
             clientId: process.env.KAFKA_CLIENT_ID,
             brokers: [
@@ -14,19 +17,30 @@ export class KafkaService {
         this.consumeMessages();
     }
 
+    async processTopic( topic: string, payload ) {
+        payload = JSON.parse(payload);
+        switch (topic) {
+            case process.env.KAFKA_USER_TOPIC:
+                if (payload.operation === 'CREATE') {
+                    this.slotService.createSlots(payload.data);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     async consumeMessages() {        
         const consumer = this.kafka.consumer({
-            groupId: process.env.KAFKA_GROUP_ID
+            groupId: process.env.KAFKA_GROUP_ID,
+            heartbeatInterval: 10000,
+            sessionTimeout: 30000,          
         });
         await consumer.connect();
         await consumer.subscribe({ topic: process.env.KAFKA_USER_TOPIC, fromBeginning: true });
         await consumer.run({
-            eachMessage: async ({ topic, partition, message }) => {
-                console.log({
-                    topic,
-                    partition,
-                    value: message.value.toString(),
-                });
+            eachMessage: async ({ topic, message }) => {
+                this.processTopic( topic, message.value.toString() );
             },
         });
     }
